a1ddd4b85664e78e16ec8a5cde77217076c3c4f6
Geraldo Fada <geraldofada.neto@gmail.com>
2022-02-08 22:23:31 -0300

    completed user create endpoint

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,15 +1,69 @@
 import { Request, Response } from 'express';
 
-// import * as jsend from '../../utils/jsend.util';
+import { PrismaClientValidationError, PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import prisma from '../../clients/prisma.client';
+
+import * as jsend from '../../utils/jsend.util';
 
 // import redis from '../../clients/redis.client';
-// import prisma from '../../clients/prisma.client';
+
+import * as validator from './user.validator';
+
+import hashString from '../../utils/crypto.utils';
 
 const login = async (req: Request, res: Response) => {};
-const signup = (req: Request, res: Response) => {};
-const get = (req: Request, res: Response) => {};
-const list = (req: Request, res: Response) => {};
-const update = (req: Request, res: Response) => {};
-const remove = (req: Request, res: Response) => {};
+const signup = async (req: Request, res: Response) => {};
 
-export { login, signup, get, list, update, remove };
+const create = async (req: Request, res: Response) => {
+  const { value, error } = validator.create.validate(req.body);
+  if (error) return jsend.fail(res, 400, error);
+
+  try {
+    const hashPassword = await hashString(value.password);
+
+    const { address, role, ...userInfo } = value;
+    const user = await prisma.user.create({
+      data: {
+        ...userInfo,
+        password: hashPassword,
+        roles: {
+          create: {
+            role,
+          },
+        },
+        addresses: {
+          create: {
+            ...address,
+          },
+        },
+      },
+    });
+
+    return jsend.success(res, 201, user);
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError && err.code === 'P2002') {
+      return jsend.fail(res, 400, err.message);
+    }
+
+    if (err instanceof PrismaClientValidationError) {
+      return jsend.fail(res, 400, err.message);
+    }
+
+    if (err instanceof Error) {
+      return jsend.error(res, 500, 'An internal error occurred.', {
+        code: 500,
+        data: err.message,
+      });
+    }
+
+    return jsend.error(res, 500, 'An internal error occurred.', null);
+  }
+};
+
+const getById = async (req: Request, res: Response) => {};
+const getByCpf = async (req: Request, res: Response) => {};
+const list = async (req: Request, res: Response) => {};
+const update = async (req: Request, res: Response) => {};
+const remove = async (req: Request, res: Response) => {};
+
+export { login, signup, create, getById, getByCpf, list, update, remove };
